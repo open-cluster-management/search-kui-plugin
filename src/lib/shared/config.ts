b2c413aef48acb3c86ebba01ebd097883e3afeb8
Marquis Hackett <M.H@ibm.com>
2019-08-12 14:57:36 -0400

    Copyright check (#36)
    * added copyright check from project template
    
    * fixed incorrect copyright headers

--- src/lib/shared/config.ts
+++ src/lib/shared/config.ts
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
- *
- * Note to U.S. Government Users Restricted Rights:
- * Use, duplication or disclosure restricted by GSA ADP Schedule
- * Contract with IBM Corp.
- *******************************************************************************/
+* Licensed Materials - Property of IBM
+* (c) Copyright IBM Corporation 2019. All Rights Reserved.
+*
+* Note to U.S. Government Users Restricted Rights:
+* Use, duplication or disclosure restricted by GSA ADP Schedule
+* Contract with IBM Corp.
+*******************************************************************************/
 import { inBrowser } from '@kui-shell/core/core/capabilities'
 
 var nconf = require('nconf')
