9396a15f0684e4370892695aae9d28ee56698d3f
Robert Howington <roberthowington@ibm.com>
2019-11-04 14:21:44 -0500

    Plugin e2e build  (#92)
    * test: init commit for e2e test refactor
    
    * new travis.yml test
    
    * test new travis
    
    * test travis
    
    * travis test
    
    * travis test
    
    * travis test- replace matrix with jobs
    
    * spacing test travis
    
    * last spacing test
    
    * spacing
    
    * add build tools makefile and updated travis
    
    * update Makefile
    
    * add eslint config
    
    * delete test_component app.js
    
    * remove linting
    
    * refactor make package for non-local
    
    * refactor package to conditional
    
    * refactor package conditional
    
    * make package refact ifneq
    
    * chg ifneq to ifeq
    
    * remove make pkg conditional
    
    * move pkging from makefile to travis.yml
    
    * add conditional to .travis.yml
    
    * add docker-logins
    
    * remove mcm-kui cli test
    
    * network config and commands refactor
    
    * remove log, add config, refactor mcm-kui build scripts
    
    * add log - determine window.location
    
    * typo fix and remove log
    
    * update search and mcm endpoints to subnet
    
    * replace make run with docker run in mcm-kui build
    
    * remove params in build*.sh and default to mcm-kui repo params
    
    * log if travis env variables are present
    
    * remove docker_run_opts from sh
    
    * fix invalid container name in build*.sh
    
    * run mcm-ui build on subnetwork and pushing to scratch config
    
    * fix network name for mcm-ui
    
    * remove any integration reference for docker push
    
    * add back release command
    
    * clean dead code and log for plugin integration test
    
    * test packagin
    
    * test packaging
    
    * remove travis conditional for packaging
    
    * package command in makefile ls to see if package exists
    
    * revert to plugin package script in travis, add download-plugins call to mcm-kui build
    
    * refactor when we replace packaged search-plugin
    
    * destructure make install to only install-proxy and client
    
    * remove download-plugins altogether and add plugin-downloads dir
    
    * replace download-plugins, verify package is being added
    
    * verified package is being added - removed log and cds
    
    * add v3, add update-plugins
    
    * clean hardcoded urls
    
    * adapted sho's changes, pull in mcm-kui test-repo
    
    * fix plugin-test paths
    
    * refactor make commands
    
    * typo and dead code
    
    * path typo
    
    * move update-plugins to makefile
    
    * actually run container this time
    
    * remove test components, clean dead code
    
    * remove eslint config
    
    * fix typo-remove tag assignment for debug
    
    * fix date typo, re-add docker_image
    
    * requested changes, test tag reassignment, remove dead code
    
    * remove docker-tag assignment, test that plugin is integrated
    
    * typo in docker_tag assignment on test pr
    
    * test env exports from travis.yml
    
    * replace make update-plugins
    
    * debug update-plugins
    
    * remove test comment-verified integration
    
    * separate test from plugin-integration
    
    * remove selenium log
    
    * typo
    
    * path for copyrightcheck
    
    * reposition copyrightcheck and create make recipe

---
+++ build-tools/build-mcm-kui.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2019. All Rights Reserved.
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+
+git clone git@github.ibm.com:IBMPrivateCloud/mcm-kui.git
+
+cd mcm-kui/
+
+make init
+make download-clis
+make download-plugins
+rm plugin-downloads/plugin-search.tgz
+mv ../../plugin-search.tgz ./plugin-downloads
+
+make -C client client-update-plugins
+
+echo "Installing proxy and client"
+make install-proxy
+make install-client
+make webpack
+make headless
+make build-image
+
+docker tag mcm-kui-proxy:latest mcm-kui-proxy:$(cat RELEASE_VERSION)
+
+make run
+
+# git clone git@github.ibm.com:IBMPrivateCloud/mcm-kui-tests.git
+# make -C mcm-kui-tests setup-dependencies
+# make -C mcm-kui-tests run-all-tests
+
+#TODO add our tests ^^
