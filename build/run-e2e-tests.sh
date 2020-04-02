fe03026b16906cd0c73003d604ff795943250a24
rhowingt@redhat.com <rhowingt@redhat.com>
2020-04-01 23:30:54 -0400

    test: update reference to oc recipes in our e2e sh script
    re #1088

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -5,4 +5,8 @@ echo "> Running tests from mcm-kui-test repo"
 export DOCKER_IMAGE_AND_TAG=${1}
 
 cd build/kui-web-terminal
+sudo make -C tests install-oc
+make -C tests login-oc
 make run-all-tests
+
+cd ../..
