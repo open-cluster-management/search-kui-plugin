75cda1f1bfb7922ab1ae40f9e8b1d6d3508e7f89
rhowingt@redhat.com <rhowingt@redhat.com>
2020-04-09 13:22:51 -0400

    test: revert command listener

--- src/src-web/controller/search.tsx
+++ src/src-web/controller/search.tsx
@@ -116,12 +116,12 @@ export const doSearch = (args) => new Promise((resolve, reject) => {
  */
 export default async (commandTree: Registrar) => {
 
-  let opts = window.location.href === 'https://localhost:8081/kui'
-  ? { usage, noAuthOk: true, inBrowserOk: false}
-  : {usage, noAuthOk: true, inBrowserOk: true}
+  let opts = {usage, noAuthOk: true, inBrowserOk: true}
 
+  // let opts = window.location.href === 'https://localhost:8081/kui'
+  // ? { usage, noAuthOk: true, inBrowserOk: false}
+  // : {usage, noAuthOk: true, inBrowserOk: true}
 
-  console.log(window.location.hostname)
   const searchCmd = commandTree.listen('/search', doSearch, opts)
   commandTree.synonym('/s', doSearch, searchCmd, opts)
 
