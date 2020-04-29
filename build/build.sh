04109deb0b4f1c14368cf2438c6f8a1ae35b05fa
rhowingt@redhat.com <rhowingt@redhat.com>
2020-04-29 02:21:23 -0400

    test: debug docker login - elucidate comp/pull

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,6 @@ echo "> Running build/build.sh"
 
 export DOCKER_IMAGE_AND_TAG=${1}
 
-docker login quay.io/open-cluster-management -u ${DOCKER_USER} -p ${DOCKER_PASS}
-make run-search-api
 make install
 make package
 make integrate-plugin
