d3076be0de4a4175147c30523e60b964a93d3aed
Robert Howington <rhowingt@redhat.com>
2020-06-03 00:08:35 -0400

    Rhowingt/v8 dev (#76)
    * fix: fix input to run rev-search/tabcompletion/search/registeredcommands
    
    removed unnecessary src-web/ folder, updated customSearchInput to not render since inputprovider
    does that for us, modified paths, minor css update(needs further work)
    
    * fix: fix setup for dev environment
    
    * build: debug - failing submoduleto connect to test repo
    
    * build: debug submodule connection failure
    
    * build: re-add kui-shell/plugin-client-common - for build
    
    * build: update package-lock for npm ci in build
    
    * build: remove buildCSS from Makefile
    
    * fix: update import ref for copy16
    
    * build: debug integration into kwt branch with hardcoded custominput prop
    
    * build: scss ref debug
    
    * build: debug css ref
    
    * build: debug css import
    
    * build: debug if hardcoded prop fails

--- build/build-kui-web-terminal.sh
+++ build/build-kui-web-terminal.sh
@@ -8,7 +8,9 @@
 
 set -e
 
-git clone --depth=50 https://github.com/open-cluster-management/kui-web-terminal.git -b rhowingt/search-plugin
+# git clone --depth=50 https://github.com/open-cluster-management/kui-web-terminal.git -b rhowingt/search-plugin
+git clone --depth=50 https://github.com/open-cluster-management/kui-web-terminal.git
+
 cd kui-web-terminal/
 
 
