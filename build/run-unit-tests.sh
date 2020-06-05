abc2be00b0639b38589f98a55cd607f1d71c768e
Robert Howington <rhowingt@redhat.com>
2020-06-05 13:07:51 -0400

    Rhowingt/v8 dev (#84)
    * build: update build and dev scripts to account for temp kwt-integration and css reference issue
    
    * build: update integrate script - remove sed since kwt hardcodes for now
    
    * build: handle deps needed for dev vs build
    
    * build: debug build on rhowingt/search-plugin branch of KWT
    
    * build: fix typo in int script
    
    * build: add babel step to build for dist/commonjs
    
    * test: mod unit test init script for css path

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -3,5 +3,9 @@ set -e
 
 echo "> Running build/run-unit-tests.sh"
 npm run compile
+cd web/scss/
+sed "s/..\/..\/..\/..\/..\//..\/..\/..\/..\//" index.scss > new-index.scss
+mv new-index.scss index.scss
+cd ..
 npm run buildCSS
 make run-unit-tests
