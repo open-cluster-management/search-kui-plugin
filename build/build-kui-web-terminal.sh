07dc326c005c7778ba7655a71969fea075dbe858
rhowingt@redhat.com <rhowingt@redhat.com>
2020-06-09 16:16:05 -0400

    build: test build within kwt

--- build/build-kui-web-terminal.sh
+++ build/build-kui-web-terminal.sh
@@ -8,17 +8,50 @@
 
 set -e
 
+cd ../..
 # git clone --depth=50 https://github.com/open-cluster-management/kui-web-terminal.git
 git clone https://github.com/open-cluster-management/kui-web-terminal.git -b rhowingt/search-plugin
 
+cp -r search-kui-plugin kui-web-terminal/client/plugins
+cd kui-web-terminal/client
+KUI_REPO_DIR=$(pwd)
+cd plugins/plugin-search
+SEARCH_PLUGIN_DIR=$(pwd)
 
-cd kui-web-terminal/
+format_title "Step 2: Configure IBM/Kui repo."
+cd $KUI_REPO_DIR
+# 2a. Update package.json 
+# ADD:    "dependencies": { "@kui-shell/plugin-search": "file:SEARCH_PLUGIN_DIR"} 
+jq '.dependencies |= . +{"@kui-shell/plugin-search": $searchPluginDir}' --arg searchPluginDir "file:plugins/plugin-search" package.json > new.package.json
+mv new.package.json package.json
+
+# 2b. Update tsconfig.json
+# "references": [{ "path": "SEARCH_PLUGIN_DIR" }]
+jq '.references += [{ "path": $searchPluginDir }]' --arg searchPluginDir 'plugins/plugin-search' tsconfig.json > new.tsconfig.json
+mv new.tsconfig.json tsconfig.json
+
+format_title "Step 3: Configure search-kui-plugin repo."
+cd $SEARCH_PLUGIN_DIR
+
+# 3a. Update tsconfig.json
+# "extends": "../../packages/builder/tsconfig-base.json",
+jq '. |= . +{"extends": $path}' --arg path "../../tsconfig.json" tsconfig.json > new.tsconfig.json
+mv new.tsconfig.json tsconfig.json
+
+
+npm pack
+mv kui-shell-plugin-search-0.0.0-semantically-released.tgz plugin-search.tgz
+
+cd $KUI_REPO_DIR/..
+
+npm i
+npx kui-compile
 
 make init
 make download-clis
 make download-plugins
 rm plugin-downloads/plugin-search.tgz
-cp ../../plugin-search.tgz ./plugin-downloads
+cp client/plugins/plugin-search/plugin-search.tgz ./plugin-downloads
 
 make -C client client-update-plugins
 
--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ echo "> Running build/build.sh"
 
 export DOCKER_IMAGE_AND_TAG=${1}
 
-npm run compile
-npx --no-install babel mdist --out-dir dist --ignore '**/*.d.ts','**/*.js.map' --no-copy-ignored
-make package
+# npm run compile
+# npx --no-install babel mdist --out-dir dist --ignore '**/*.d.ts','**/*.js.map' --no-copy-ignored
+# make package
 make integrate-plugin
