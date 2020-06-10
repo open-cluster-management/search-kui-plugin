0eaeb7b0f7cbd89d53332b23ef7d0da273f46dab
rhowingt@redhat.com <rhowingt@redhat.com>
2020-06-10 10:02:25 -0400

    build: revert css ref

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -4,7 +4,7 @@ set -e
 echo "> Running build/run-unit-tests.sh"
 make compile-plugin
 cd web/scss/
-sed "s/..\/..\/..\/..\/..\//..\/..\/..\/..\//" index.scss > new-index.scss
+sed "s/..\/..\/..\/..\/..\//..\/..\//" index.scss > new-index.scss
 mv new-index.scss index.scss
 cd ..
 npm run buildCSS
