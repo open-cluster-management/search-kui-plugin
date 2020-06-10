911d02672ca4d7eaaa97a883bf8659960e8d8e46
Robert Howington <rhowingt@redhat.com>
2020-06-10 11:24:46 -0400

    Rhowingt/v8 dev (#114)
    * build: css ref debug
    
    * refactor: remove dead injectcss and tests update async babel
    
    * test: update unit-test dir

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -3,9 +3,11 @@ set -e
 
 echo "> Running build/run-unit-tests.sh"
 make compile-plugin
+
 cd web/scss/
 sed "s/..\/..\/..\/..\/..\//..\/..\//" index.scss > new-index.scss
 mv new-index.scss index.scss
-cd ..
+
+cd ../..
 npm run buildCSS
 make run-unit-tests
