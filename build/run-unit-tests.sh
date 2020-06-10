aa8bc23f1a5e2c22c1eadb8b8c3f9592453b0c5e
dislbenn <lavontae.bennett@gmail.com>
2020-06-10 15:49:54 -0400

    added placeholder in unit test script

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-# echo "> Running build/run-unit-tests.sh"
+echo "> Running build/run-unit-tests.sh"
+echo "!!!!! Unit tests are currently disabled. Re-enable in ./build/run-unit-tests.sh !!!!"
+
 # make compile-plugin
 
 # cd web/scss/
