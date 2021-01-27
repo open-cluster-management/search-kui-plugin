c2a2df3053873469f7c66dc9cca5c9b16dd1e41a
rhowingt@redhat.com <rhowingt@redhat.com>
2021-01-27 13:10:56 -0500

    update acm endpoint for token fetch

--- src/lib/shared/config.ts
+++ src/lib/shared/config.ts
@@ -1,94 +1,95 @@
 /*******************************************************************************
-* 
-* Copyright (c) 2020 Red Hat, Inc.
-* 
-* Licensed Materials - Property of IBM
-* (c) Copyright IBM Corporation 2019. All Rights Reserved.
-*
-* Note to U.S. Government Users Restricted Rights:
-* Use, duplication or disclosure restricted by GSA ADP Schedule
-* Contract with IBM Corp.
-*******************************************************************************/
+ *
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2019. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
 import { inBrowser } from '@kui-shell/core'
 
 let staticConfig
-if (process.env.NODE_ENV === 'development'){
-  staticConfig = require('./search.json')
+if (process.env.NODE_ENV === 'development') {
+	staticConfig = require('./search.json')
 }
 
 const getXsrfToken = () => {
-  const token = document.querySelector('#app-access') ? document.querySelector('#app-access')['value'] : ''
-  return token.toString('ascii')
+	const token = document.querySelector('meta') ? document.querySelector('meta').content : ''
+	return token.toString()
 }
 
 interface StaticConfig {
-  env: string
-  SEARCH_API: string
-  CONSOLE_API: string
+	env: string
+	SEARCH_API: string
+	CONSOLE_API: string
 }
 
 interface AuthConfig {
-  authorization: string
-  cookie?: string
+	authorization: string
+	cookie?: string
 }
 
 export type Config = StaticConfig &
-  AuthConfig & {
-    xsrfToken: string
-  }
+	AuthConfig & {
+		xsrfToken: string
+	}
 
 export async function getConfig(): Promise<Config> {
+	const nconf = require('nconf')
+	const WHITELIST = ['contextPath']
 
-  const nconf = require('nconf')
-  const WHITELIST = ['contextPath']
+	let config: Config = Object.assign(
+		{
+			env: '',
+			// Electron needs to grab backend urls somehow  Ex: https://<cluster-ip>:<backend-port>/(searchapi || hcmuiapi)/graphql
+			// Browser can grab backend urls from the window.location.origin
+			SEARCH_API: staticConfig
+				? staticConfig.SEARCH_API
+				: `${
+						window && window.location && window.location.origin
+				  }/multicloud/search/graphql`,
+			CONSOLE_API: staticConfig
+				? staticConfig.CONSOLE_API
+				: `${window && window.location && window.location.origin}/multicloud/graphql`,
+		},
+		{
+			// Browser needs xsrf token for requests
+			xsrfToken: inBrowser() ? getXsrfToken() : null,
 
-  let config: Config = Object.assign(
-    {
-      env: '',
-      // Electron needs to grab backend urls somehow  Ex: https://<cluster-ip>:<backend-port>/(searchapi || hcmuiapi)/graphql
-      // Browser can grab backend urls from the window.location.origin
-      SEARCH_API: staticConfig 
-      ? staticConfig.SEARCH_API
-      : `${window && window.location && window.location.origin}/multicloud/search/graphql`,
-      CONSOLE_API: staticConfig
-      ? staticConfig.CONSOLE_API
-      : `${window && window.location && window.location.origin}/multicloud/graphql`,
-    },
-    {
-      // Browser needs xsrf token for requests
-      xsrfToken: inBrowser() ? getXsrfToken() : null,
+			// Electron needs the user access token
+			authorization: 'Bearer ',
+			cookie: 'cfc-cookie-access-token=',
+		}
+	)
 
-        // Electron needs the user access token
-      authorization: 'Bearer ',
-      cookie: 'cfc-cookie-access-token=',
-    },
-  )
+	if (process.env.NODE_ENV === 'development') {
+		try {
+			const authConfig: AuthConfig = require('./search-auth.json')
+			if (authConfig.authorization) {
+				config.authorization = authConfig.authorization
+				config.cookie = authConfig.cookie
+			}
+		} catch (err) {
+			console.error(err)
+		}
+	}
 
-  if (process.env.NODE_ENV === 'development') {
-    try {
-      const authConfig: AuthConfig = require('./search-auth.json')
-      if (authConfig.authorization) {
-        config.authorization = authConfig.authorization
-        config.cookie = authConfig.cookie
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  }
+	if (nconf) {
+		WHITELIST.forEach((i) => {
+			config[i] = nconf.get(i)
+		})
+		if (process.env.NODE_ENV) {
+			config.env = process.env.NODE_ENV
+		}
+	} else {
+		const configElement = document.getElementById('config')
+		config = (configElement && JSON.parse(configElement.textContent)) || {}
+	}
 
-  if (nconf) {
-    WHITELIST.forEach( i => {
-      config[i] = nconf.get(i)
-    })
-    if (process.env.NODE_ENV){
-      config.env = process.env.NODE_ENV
-    }
-  } else {
-    const configElement = document.getElementById('config')
-    config = (configElement && JSON.parse(configElement.textContent)) || {}
-  }
-
-  return config
+	return config
 }
 
 export default getConfig
--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* 
-* Copyright (c) 2020 Red Hat, Inc.
-* 
-* Licensed Materials - Property of IBM
-* (c) Copyright IBM Corporation 2019. All Rights Reserved.
-*
-* Note to U.S. Government Users Restricted Rights:
-* Use, duplication or disclosure restricted by GSA ADP Schedule
-* Contract with IBM Corp.
-*******************************************************************************/
+ *
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2019. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
 
 import { CapabilityRegistration, inBrowser } from '@kui-shell/core'
 import getConfig from './lib/shared/config'
@@ -19,24 +19,24 @@ import { GET_SEARCH_SCHEMA } from './definitions/search-queries'
 
 // Register searchBarWrapper
 const registerCapability: CapabilityRegistration = async () => {
-  if (inBrowser() && (await getConfig()).env !== 'development') {
-    // Get user token from browser
-    fetch('/multicloud/search')
-    .then((page) => page.text())
-    .then((data) => {
-      const dom = new DOMParser().parseFromString(data, 'text/html')
-      const access = dom.querySelector('#app-access')
-      document.querySelector('body').appendChild(access)
-    })
-  }
+	if (inBrowser() && (await getConfig()).env !== 'development') {
+		// Get user token from browser
+		fetch('/search')
+			.then((page) => page.text())
+			.then((data) => {
+				const dom = new DOMParser().parseFromString(data, 'text/html')
+				const token = dom.querySelector('meta')
+				document.querySelector('body').appendChild(token)
+			})
+	}
 
-  HTTPClient('post', 'search', GET_SEARCH_SCHEMA)
-  .then((resp) => {
-    setPluginState('searchSchema', lodash.get(resp, 'data.searchSchema.allProperties', ''))
-  })
-  .catch((err) => {
-    setPluginState('error', err)
-  })
+	HTTPClient('post', 'search', GET_SEARCH_SCHEMA)
+		.then((resp) => {
+			setPluginState('searchSchema', lodash.get(resp, 'data.searchSchema.allProperties', ''))
+		})
+		.catch((err) => {
+			setPluginState('error', err)
+		})
 }
 
 export default registerCapability
